bbe05cf10effd19cd46873cf8e1b5678cae044c1
Jose Aylas Flores <joseflores316@gmail.com>
2023-05-16 12:07:16 +0200

    Añado la configuración básica de seguridad.

--- src/main/java/com/cronos/customer/CustomerApplication.java
+++ src/main/java/com/cronos/customer/CustomerApplication.java
@@ -2,9 +2,9 @@ package com.cronos.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CustomerApplication {
 
     public static void main(String[] args) {
